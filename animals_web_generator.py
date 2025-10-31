1b82e4584bbcb8bed595c08f0c95a810f6d78bf6
Patrick Mashegwana <patrickmashegwana@gmail.com>
2025-10-31 13:21:48 +0100

    Improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -16,24 +16,31 @@ def write_html_file(new_html):
     with open("animals.html", "w") as file:
         file.write(new_html)
 
-def print_animal_data(animals_data):
+def create_animals_html(animals_data):
     """Iterates through animals and prints selected info"""
     output = ""
     for animal in animals_data:
         output += '<li class="cards__item">'
-        output += f"Name: {animal['name']}<br/>\n"
-        output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
-        output += f"Location: {animal['locations'][0]}<br/>\n"
+        output += (f'<div class="card__title">{animal["name"]} '
+                   f'({animal["taxonomy"]["scientific_name"]})</div>\n')
+        if 'slogan' in animal['characteristics']:
+            output += f'<p class="card__text"><em>{animal["characteristics"]["slogan"]}</em></p>'
+        output += '<div class="card__text">'
+        output += '<ul>'
+        output += f"<li><strong>Diet:</strong> {animal['characteristics']['diet']}</li>\n"
+        output += f"<li><strong>Location:</strong> {animal['locations'][0]}</li>\n"
         if 'type' in animal['characteristics']:
-            output += f"Type: {animal['characteristics']['type']}<br/>\n"
-        output += '</li>'
+            output += f"<li><strong>Type:</strong> {animal['characteristics']['type']}</li>\n"
+        output += '</ul>'
+        output += "</div>"
+        output += "</li>"
 
     return output
 
 if __name__ == "__main__":
     animals_data = load_data("animals_data.json")
     result = read_html_file()
-    data = print_animal_data(animals_data)
+    data = create_animals_html(animals_data)
 
     result = result.replace("__REPLACE_ANIMALS_INFO__", data)
 
